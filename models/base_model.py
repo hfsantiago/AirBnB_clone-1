8506148d44230d666dce5f8662d3dca67bf1ecd8
Esteban1891 <esteban.delahoz15@gmail.com>
2020-08-13 20:04:10 -0500

    add task 6

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 """This module defines a base class for all models in our hbnb clone"""
+#!/usr/bin/python3
+
+"""This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime
